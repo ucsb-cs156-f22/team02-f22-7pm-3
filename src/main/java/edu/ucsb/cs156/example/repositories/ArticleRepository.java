5637889fe36ace93e67ec6b0e859e00837d705e7
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-10-25 17:02:21 -0700

    Fix article repository type

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
@@ -1,12 +1,12 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.User;
+import edu.ucsb.cs156.example.entities.Article;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ArticleRepository extends CrudRepository<User, Long> {
+public interface ArticleRepository extends CrudRepository<Article, Long> {
     
 }
