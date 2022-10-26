a466ac02660ebfe3f5ab4631ca7e54449c7234dd
Blake Randle <randle@ucsb.edu>
2022-10-25 17:56:36 -0700

    br - create controller class for organizations with /all and /post endpoints

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -0,0 +1,63 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.entities.UCSBOrganization;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Api(description = "UCSBOrganizations")
+@RequestMapping("/api/ucsborganizations")
+@RestController
+@Slf4j
+public class UCSBOrganizationController extends ApiController {
+
+    @Autowired
+    UCSBOrganizationRepository ucsbOrganizationRepository;
+
+    @ApiOperation(value = "List all ucsb organizations")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<UCSBOrganization> allUCSBOrganizations() {
+        Iterable<UCSBOrganization> orgs = ucsbOrganizationsRepository.findAll();
+        return orgs;
+    }
+
+    @ApiOperation(value = "Create a new organization")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public UCSBOrganization postUCSBOrganization(
+            @ApiParam("orgCode") @RequestParam String orgCode,
+            @ApiParam("orgTranslationShort") @RequestParam String orgTranslationShort,
+            @ApiParam("orgTranslation") @RequestParam String orgTranslation,
+            @ApiParam("inactive") @RequestParam boolean inactive)
+            {
+
+            UCSBOrganization org = new UCSBOrganization();
+            org.setOrgCode(orgCode);
+            org.setOrgTranslationShort(orgTranslationShort);
+            org.setOrgTranslation(orgTranslation);
+            org.setInactive(inactive);
+    
+            UCSBOrganization savedOrg = UCSBOrganizationRepository.save(org);
+    
+            return savedOrg;
+    }
+}
