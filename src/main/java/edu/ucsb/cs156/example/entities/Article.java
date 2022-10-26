15bbf6d15efb26891e0ab4637430de16cfa0c28f
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-10-25 17:15:20 -0700

    Change access on article no-arg constructor to pub

--- src/main/java/edu/ucsb/cs156/example/entities/Article.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Article.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Builder
 @Entity(name = "articles")
 public class Article {
