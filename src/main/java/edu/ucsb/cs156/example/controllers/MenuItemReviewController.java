9794d28d33a5cd1c110abd205560f374656c1bfc
wesleykam <72772860+wesleykam@users.noreply.github.com>
2022-11-01 01:55:57 -0700

    Add DELETE endpoint for MenuItemReview (#72)
    * Add get single item endpoint
    
    * Add Delete endpoint MenuItemReview
    
    * merge

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -39,7 +39,7 @@ public class MenuItemReviewController extends ApiController {
     @ApiOperation(value = "List all menu item reviews")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
-    public Iterable<MenuItemReview> allUCSBDates() {
+    public Iterable<MenuItemReview> allMenuItemReviews() {
         Iterable<MenuItemReview> reviews = menuItemReviewRepository.findAll();
         return reviews;
     }
@@ -82,5 +82,18 @@ public class MenuItemReviewController extends ApiController {
         MenuItemReview savedMenuItemReview = menuItemReviewRepository.save(menuItemReview);
 
         return savedMenuItemReview;
+    }
+
+    // DELETE
+    @ApiOperation(value = "Delete a MenuItemReview")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteMenuItemReview(
+            @ApiParam("id") @RequestParam Long id) {
+        MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+        menuItemReviewRepository.delete(menuItemReview);
+        return genericMessage("MenuItemReview with id %s deleted".formatted(id));
     }
 }
